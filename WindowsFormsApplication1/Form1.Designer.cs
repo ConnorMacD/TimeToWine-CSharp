3ec0807451cd70373d8727dbd411bef98b9bb208
Connor MacDonald <cmacd.94@gmail.com>
2016-02-22 15:26:23 -0400

    initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.wineL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grabL = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.countdown = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // wineL
+            // 
+            this.wineL.AutoSize = true;
+            this.wineL.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wineL.Location = new System.Drawing.Point(42, 9);
+            this.wineL.Name = "wineL";
+            this.wineL.Size = new System.Drawing.Size(37, 39);
+            this.wineL.TabIndex = 0;
+            this.wineL.Text = "0";
+            this.wineL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Zammy Wine";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Grab CD";
+            // 
+            // grabL
+            // 
+            this.grabL.AutoSize = true;
+            this.grabL.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grabL.Location = new System.Drawing.Point(42, 76);
+            this.grabL.Name = "grabL";
+            this.grabL.Size = new System.Drawing.Size(37, 39);
+            this.grabL.TabIndex = 2;
+            this.grabL.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(40, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // countdown
+            // 
+            this.countdown.Location = new System.Drawing.Point(67, 149);
+            this.countdown.MaxLength = 4;
+            this.countdown.Name = "countdown";
+            this.countdown.Size = new System.Drawing.Size(51, 20);
+            this.countdown.TabIndex = 7;
+            this.countdown.TabStop = false;
+            this.countdown.WordWrap = false;
+            this.countdown.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.countdown_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(131, 209);
+            this.Controls.Add(this.countdown);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.grabL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.wineL);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label wineL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label grabL;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox countdown;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        int timeleft;
+        Boolean wine = true;
+        Boolean grab = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+
+
+        private void countdown_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (wine)
+            {
+                if (timeleft > 0)
+                {
+                    // Display the new time left
+                    // by updating the Time Left label.
+                    timeleft -= 1;
+                    Console.Write(timeleft);
+                    wineL.Text = timeleft + "";
+                }
+                else
+                {
+                    wineL.Text = timeleft + "";
+                    timer1.Stop();
+                    timeleft = 3;
+                    grabL.Text = timeleft + "";
+                    wine = false;
+                    grab = true;
+                    timer1.Start();
+                }
+            }
+            if (grab)
+            {
+                if (timeleft > 0)
+                {
+                    // Display the new time left
+                    // by updating the Time Left label.
+                    timeleft -= 1;
+                    
+                    grabL.Text = timeleft + "";
+                }
+                else
+                {
+                    grabL.Text = timeleft + "";
+                    timer1.Stop();
+                    timeleft = Int32.Parse(countdown.Text);
+                    wineL.Text = timeleft + "";
+                    wine = true;
+                    grab = false;
+                    timer1.Start();
+                }
+            }
+        }
+        
+        //start
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timeleft = Int32.Parse(countdown.Text);
+            countdown.Enabled = false;
+            wineL.Text = timeleft + "";
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        //stop
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timeleft = 0;
+            wineL.Text = "0";
+            grabL.Text = "0";
+            Boolean wine = true;
+            Boolean grab = false;
+            countdown.Enabled = true;
+            timer1.Stop();
+        }
+
+
+    }
+}
